c075a1b431d4be02a4bc49583f870602f5625ce5
kosarz-endehboodi <imkosarzbdm@gmail.com>
2023-07-21 03:43:14 +0330

    added the search Engien

--- src/appp.js
+++ src/appp.js
@@ -1,8 +1,4 @@
 //var
-//api key and url
-let apiKey = "c5135fc130d435646a416541ffe05185";
-let city = "new york";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //element
 let temperatureEl = document.getElementById("temperature");
@@ -13,26 +9,30 @@ let windEl = document.getElementById("wind");
 let dateEl = document.getElementById("date");
 let iconEl = document.getElementById("icon");
 //console.log(apiUrl);
+//city form
+let cityFormEl = document.getElementById("searchForm");
+
+cityFormEl.addEventListener("submit", searchCity);
 
 function FormDate(timestamp) {
   //calculate the date
   let time = new Date(timestamp);
-  let hour = time.getHours();
+  let hours = time.getHours();
   let minutes = time.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  if (hour < 10) {
-    hour = `0${hour}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
 
   let day = time.getDay();
   let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-  return `${days[day]} ${hour}:${minutes}`;
+  return `${days[day]} ${hours}:${minutes}`;
 }
 
 function displayTempCity(response) {
- // console.log(response.data);
+  // console.log(response.data);
 
   windEl.innerText = response.data.wind.speed;
   humidityEl.innerText = response.data.main.humidity;
@@ -40,10 +40,27 @@ function displayTempCity(response) {
   cityEl.innerText = response.data.name;
   temperatureEl.innerText = Math.round(response.data.main.temp);
   dateEl.innerText = FormDate(response.data.dt * 1000);
+  console.log(FormDate(response.data.dt * 1000));
   let iconIdpng = response.data.weather[0].icon;
   iconEl.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${iconIdpng}@2x.png`
   );
 }
-axios.get(apiUrl).then(displayTempCity);
+
+// function api key and url
+function search(city) {
+  //api key and url
+  let apiKey = "c5135fc130d435646a416541ffe05185";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTempCity);
+}
+search("new york");
+
+function searchCity(e) {
+  e.preventDefault();
+  let formContorol = document.getElementById("formContorol");
+  let cityvalue = formContorol.value;
+  search(cityvalue);
+  // console.log(cityvalue);
+}
